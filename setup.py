115ce3f6283bdffb5cfd46eb30f6706abee0ca34
hvelarde <hector.velarde@gmail.com>
2013-09-23 13:49:47 -0300

    Update package distribution

--- setup.py
+++ setup.py
@@ -1,23 +1,25 @@
-from setuptools import setup, find_packages
-import os
+from setuptools import find_packages
+from setuptools import setup
 
 version = '1.0a3'
 
 setup(name='plone.app.workflowmanager',
       version=version,
-      description="a workflow manager for plone",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      description="A workflow manager for Plone",
+      long_description=(
+          open("README.rst").read() + "\n" +
+          open("CHANGES.rst").read()
+      ),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
+          "Framework :: Plone",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
       keywords='plone workflow manager',
       author='Nathan Van Gheem',
       author_email='plone-developers@lists.sourceforge.net',
-      url='http://svn.plone.org/svn/plone/plone.app.example',
+      url='https://github.com/plone/plone.app.workflowmanager',
       license='GPL',
       packages=find_packages('src'),
       package_dir={'': 'src'},
@@ -29,7 +31,7 @@ setup(name='plone.app.workflowmanager',
           'setuptools',
           'plone.app.jquery>=1.7'
       ],
-      extras_require = {
+      extras_require={
           'test': [
               'plone.app.testing',
               'interlude',
