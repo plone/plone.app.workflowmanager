b48bf7ad8bb8c093123cfaadc7221b885f3820cc
Thomas Desvenain <thomas.desvenain@gmail.com>
2014-02-19 13:00:01 +0100

    Preparing release 1.0rc4

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-1.0rc4 (unreleased)
+1.0rc4 (2014-02-19)
 -------------------
 
 - Nothing changed yet.
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '1.0rc4.dev0'
+version = '1.0rc4'
 
 setup(name='plone.app.workflowmanager',
       version=version,
